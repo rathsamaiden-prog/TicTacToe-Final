d51cba15d3441eecbd19c189aafca664cee6a7f5
Aiden Rathsam <rathsamaiden@gmail.com>
2025-10-16 18:17:04 -0400

    game almost functional - dont have img for selection and reset needs tweaked

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-let board = [
-    [1,`o`], [2,null], [3,null],
-    [4,null], [5,`x`], [6,null],
-    [7,`x`], [8,null], [9,null]
-]
-const winCombos = [
-    [1,2,3], [4,5,6], [7,8,9],
-    [1,5,6], [7,5,3]
-]
-
-let player = {one: `x`, two: `o`}
-let currentPlayer = player.one
+let board;
+let winCombos;
+let player;
+let currentPlayer;
+let benchPlayer;
+reset()
 
 function placeTile(cell){
     let cellLoc = cell.id
     if(board[cellLoc-1][1] != null)
         return
-    board[cellLoc-1][1] = currentPlayer
+    board[cellLoc-1][1] = currentPlayer[1]
     console.log(board)
-    checkLine(currentPlayer)
-    if(currentPlayer === `x`)
+    checkLine(currentPlayer[1])
+    if(currentPlayer[1] === `X`){
         currentPlayer = player.two
-    else
+        benchPlayer = player.one
+    }else{
         currentPlayer = player.one
+        benchPlayer = player.two
+    }
+    setUIVals(cell)
+}
+
+function setUIVals(cell){
+    cell.children.src = `img/${currentPlayer[1]}.png`
+    document.getElementById(`status`).innerHTML = `Player ${currentPlayer[0]}: ${currentPlayer[1]}`
+    if(benchPlayer[2]){
+        benchPlayer[3] += 1
+        document.getElementById(`p${benchPlayer[0]}score`).innerHTML = benchPlayer[3]
+        reset()
+    }
 }
 
 function checkLine(currentPlayer){
@@ -32,12 +40,34 @@ function checkLine(currentPlayer){
     })
     compareCheck(movesLoc)
 }
+
 function compareCheck(movesLoc){
-    winCombos.forEach(combo => function(){
+    winCombos.forEach(function(combo){
         let containNum = 0
-        combo.forEach(num => function(){
-            if(movesLoc.includes(num)) containNum++
-            if(containsNum === 3) return `win`
+        combo.forEach(function(num){
+            if(movesLoc.includes(num)) containNum += 1
+            if(containNum === 3) currentPlayer[2] = true
         })
+    })
+}
+
+function reset(){
+    board = [
+        [1,null], [2,null], [3,null],
+        [4,null], [5,null], [6,null],
+        [7,null], [8,null], [9,null]
+    ]
+    winCombos = [
+        [1,2,3], [4,5,6], [7,8,9],
+        [1,4,7], [2,5,8], [3,6,9],
+        [1,5,6], [7,5,3]
+    ]
+
+    player = {one: [1,`X`,false, 0], two: [2,`O`,false, 0]}
+    currentPlayer = player.one
+    benchPlayer = player.two
+    let btns = document.querySelectorAll(`.row-button`)
+    btns.forEach(btn => function(){
+        btn.src = ``
     })
 }
